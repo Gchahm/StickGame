d9baf736a35d3195867a7dcceb01e6fd329b1234
Igor Loborec <igorloborec@microsoft.com>
2023-09-30 13:48:05 +0100

    More audio stuff

--- Igor/AudioScene.gd
+++ Igor/AudioScene.gd
@@ -11,14 +11,24 @@ func _process(delta):
 	pass
 
 func PauseMusic():
-	$AudioStreamPlayer.stream_paused = true
+	$Music.stream_paused = true
 
 func ResumeMusic():
-	$AudioStreamPlayer.stream_paused = false
+	$Music.stream_paused = false
 	
 func StartMusic():
-	$AudioStreamPlayer.play()
-	$AudioStreamPlayer.stream_paused = false
+	$Music.play()
+	$Music.stream_paused = false
+	
+func StopMusic():
+	$Music.stop()
+	$Music.stream_paused = false
+
+func StartAmbientSounds():
+	$Ambience.play()
+	
+func StopAmbientSounds():
+	$Ambience.stop()
 
 func Eat():
-	$Slurp.play()
+	$Slurp.play() 
