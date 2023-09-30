4f6ab41f220d11666e3ec2052683f157afce4909
guipazos <45574636+guipazos@users.noreply.github.com>
2023-09-30 14:50:54 +0100

    Motivational Words + Fire Adjust

--- Scripts/main_score.gd
+++ Scripts/main_score.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-var score: int = 100
+var score: int = 0
 var level: int = 1
 var highscore
+var motivationalWords = ["Madonna", "Amazing", "Amoeba", "Unbeliveable", "So good!", "Chicken", "Aminoacid"]  
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
@@ -15,14 +16,27 @@ func _ready():
 		save_game()
 
 func _on_plus_score_pressed():
-	score += 1000
-	$Fire/GPUParticles2D.process_material.initial_velocity_min += 50
+	score += 100
+	$ColorRect/MarginContainer/Fire/GPUParticles2D.process_material.initial_velocity_min += 10
 	$ColorRect/MarginContainer/HBoxContainer/Score_Value.text = str(score)
+		
+	if score % 500 == 0:  
+		var randomIndex = randi() % motivationalWords.size()  
+		var randomMotivationalWord = motivationalWords[randomIndex]  
+		$ColorRect/TempMessage.text = randomMotivationalWord  # Show the score text  
+		$ColorRect/TempMessage.visible = true  # Show the score text  
+		$scoreTextTimer.start()  # Start the timer
+		print ("timer started")
+
+func _on_score_text_timer_timeout():
+	print ("timer ended")
+	$ColorRect/TempMessage.visible = false
+
 
 
 func _on_minus_score_pressed():
 	score -= 1000
-	$Fire/GPUParticles2D.process_material.initial_velocity_min -= 50
+	$ColorRect/MarginContainer/Fire/GPUParticles2D.process_material.initial_velocity_min -= 50
 	$ColorRect/MarginContainer/HBoxContainer/Score_Value.text = str(score)
 
 
@@ -45,3 +59,4 @@ func _on_game_over_pressed():
 	
 	save_game()
 	$GameOverScreen.visible = true
+
